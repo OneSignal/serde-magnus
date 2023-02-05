45f09ee8974b30e3aabfc3da0bbd42e35ac57ac9
George Claghorn <george.claghorn@gmail.com>
2023-02-05 13:41:40 -0500

    Test handling arrays containing nil

--- tests/deserializing_tuples.rs
+++ tests/deserializing_tuples.rs
@@ -5,11 +5,23 @@ use serde_magnus::deserialize;
 fn test_deserializing_tuples() -> Result<(), Error> {
     let _cleanup = unsafe { magnus::embed::init() };
 
+    // From nil
     assert_eq!((), deserialize(QNIL)?);
 
+    // From a homegeneous array
+    let input: RArray = eval!("[ 123, 456, 789 ]")?;
+    let output: (i64, i64, i64) = deserialize(input)?;
+    assert_eq!((123, 456, 789), output);
+
+    // From a heterogeneous array
     let input: RArray = eval!("[ 123, true, 'Hello, world!' ]")?;
     let output: (i64, bool, String) = deserialize(input)?;
     assert_eq!((123, true, "Hello, world!".into()), output);
+
+    // From an array containing nil
+    let input: RArray = eval!("[ 123, nil, 'Hello, world!' ]")?;
+    let output: (i64, (), String) = deserialize(input)?;
+    assert_eq!((123, (), "Hello, world!".into()), output);
 
     Ok(())
 }
--- tests/serializing_tuples.rs
+++ tests/serializing_tuples.rs
@@ -5,11 +5,23 @@ use serde_magnus::serialize;
 fn test_serializing_tuples() -> Result<(), Error> {
     let _cleanup = unsafe { magnus::embed::init() };
 
+    // To nil
     let output: Value = serialize(&())?;
     assert!(output.is_nil());
 
+    // To a homogeneous array
     let output: RArray = serialize(&(1, 2, 3))?;
     assert!(eval!("output == [ 1, 2, 3 ]", output)?);
+
+    // To a heterogeneous array
+    let input: (u64, bool, &str) = (1234, true, "Hello, world!");
+    let output: RArray = serialize(&input)?;
+    assert!(eval!("output == [ 1234, true, 'Hello, world!' ]", output)?);
+
+    // To an array containing nil
+    let input: (u64, (), &str) = (1234, (), "Hello, world!");
+    let output: RArray = serialize(&input)?;
+    assert!(eval!("output == [ 1234, nil, 'Hello, world!' ]", output)?);
 
     Ok(())
 }
